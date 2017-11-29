529aee9d9d9a40b023cffd623317e7dc11b18204
wangzhenjun <wangzhenjun@okjiaoyu.cn>
2017-11-29 18:30:10 +0800

    更新代码

---
+++ src/main/java/com/company/project/eureka/Bootstrap.java
@@ -0,0 +1,14 @@
+package com.company.project.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Bootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bootstrap.class, args);
+    }
+}
