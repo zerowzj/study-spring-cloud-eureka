fe212138225f11871c0960426d516235f0772734
wangzhenjun <wangzhenjun@okay.cn>
2019-08-06 12:55:53 +0800

    更新代码

--- src/main/java/study/springcloud/eureka/Startup.java
+++ src/main/java/study/springcloud/eureka/Startup.java
@@ -1,4 +1,4 @@
-package com.company.project.eureka;
+package study.springcloud.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
