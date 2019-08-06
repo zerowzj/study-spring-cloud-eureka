98f1317d8e0fe153aa63f92b9b11276abe944a79
wangzhenjun <wangzhenjun@okay.cn>
2019-08-06 18:04:52 +0800

    更新代码

--- src/main/java/study/springcloud/eureka/support/config/SpringBootCfg.java
+++ src/main/java/study/springcloud/eureka/support/config/SpringBootCfg.java
@@ -3,7 +3,8 @@ package study.springcloud.eureka.support.config;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
 @EnableEurekaServer
+@SpringBootApplication
 public class SpringBootCfg {
+
 }
