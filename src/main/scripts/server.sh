448140181f1205e23f03f4e2ac0d48105c7741c4
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-10-23 16:58:35 +0800

    修改代码

--- src/main/scripts/server.sh
+++ src/main/scripts/server.sh
@@ -2,13 +2,21 @@ $! /bin/bash
 
 JAR_FILE=/app/study-springcloud-eureka-1.0.jar
 
-JAVA_MEM_OPTS=" -server -Xms1g -Xmx2g -Xmn1g -Xss1g -XX:PermSize=128m -XX:MaxPermSize=512m"
+JAVA_MEM_OPTS=" -server -Xms512M -Xmx512M -Xmn1g -Xss1g -XX:PermSize=128m -XX:MaxPermSize=512m"
 JAVA_GC_OPTS=" -XX:+PrintGC -XX:PrintGCDetails -XX:PrintGCTime"
 JAVA_OPTS=$JAVA_MEM_OPTS
 
-case $1 in
-start) ;;
+get_pid() {
+    pid=$(ps -ef |grep $JAR_FILE |grep -v grep |awk '{ print $2 }')
+    echo "$pid"
+}
 
+case $1 in
+start)
+  if [ -n '']; then
+    nohup java $JAVA_OPTS -jar $JAR_FILE &
+  fi
+  ;;
 stop) ;;
 
 *)
