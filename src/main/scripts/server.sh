f5d133eabb9ae8958b39ac44c005228fb975b54c
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-10-23 16:35:55 +0800

    修改代码

--- src/main/scripts/server.sh
+++ src/main/scripts/server.sh
@@ -1,3 +1,17 @@
-!$/bin/sh
+$! /bin/bash
 
-HOME=
+JAR_FILE=/app/study-springcloud-eureka-1.0.jar
+
+JAVA_MEM_OPTS=" -server -Xms1g -Xmx2g -Xmn1g -Xss1g -XX:PermSize=128m -XX:MaxPermSize=512m"
+JAVA_GC_OPTS=" -XX:+PrintGC -XX:PrintGCDetails -XX:PrintGCTime"
+JAVA_OPTS=$JAVA_MEM_OPTS
+
+case $1 in
+start) ;;
+
+stop) ;;
+
+*)
+  echo "Usage: $0{start|stop}"
+  ;;
+esac
