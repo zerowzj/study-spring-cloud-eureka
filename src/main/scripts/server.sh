05c58c119717b0763d2ed49d4558194ec116a0b9
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-10-23 17:32:18 +0800

    修改代码

--- src/main/scripts/server.sh
+++ src/main/scripts/server.sh
@@ -1,4 +1,4 @@
-$! /bin/bash
+#!/bin/bash
 
 JAR_FILE=/app/study-springcloud-eureka-1.0.jar
 
@@ -6,20 +6,34 @@ JAVA_MEM_OPTS=" -server -Xms512M -Xmx512M -Xmn1g -Xss1g -XX:PermSize=128m -XX:Ma
 JAVA_GC_OPTS=" -XX:+PrintGC -XX:PrintGCDetails -XX:PrintGCTime"
 JAVA_OPTS=$JAVA_MEM_OPTS
 
+export JAVA_HOME
+export PATH=$PATH:$JAVA_HOME/bin
+
 get_pid() {
-    pid=$(ps -ef |grep $JAR_FILE |grep -v grep |awk '{ print $2 }')
-    echo "$pid"
+  pid=$(ps -ef | grep $JAR_FILE | grep -v grep | awk '{ print $2 }')
+  echo "$pid"
+}
+start() {
+  pid=$(get_pid)
+  if [ -z "$pid" ]; then
+    nohup java $JAVA_OPTS -jar $JAR_FILE &
+  fi
+}
+stop() {
+  pid=$(get_pid)
+  if [ -n "$pid" ]; then
+      kill -9 $pid
+  fi
 }
 
 case $1 in
 start)
-  if [ -n '']; then
-    nohup java $JAVA_OPTS -jar $JAR_FILE &
-  fi
+  start
   ;;
-stop) ;;
-
+stop)
+  stop
+  ;;
 *)
-  echo "Usage: $0{start|stop}"
+  echo "Usage: $0 {start|stop}"
   ;;
 esac
