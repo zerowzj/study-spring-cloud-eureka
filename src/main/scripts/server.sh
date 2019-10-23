f575727f8620a0ad268b983190976741de1d7db8
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-10-23 18:17:12 +0800

    修改代码

--- src/main/scripts/server.sh
+++ src/main/scripts/server.sh
@@ -6,7 +6,7 @@ LOG_FILE=/app/stdout.log
 JAVA_MEM_OPTS=" -server -Xms512M -Xmx512M -Xmn128M -Xss128M -XX:PermSize=128M -XX:MaxPermSize=512M"
 JAVA_GC_OPTS=" -XX:+PrintGC -XX:PrintGCDetails -XX:PrintGCTime"
 JAVA_OPTS=$JAVA_MEM_OPTS
-
+#
 #export JAVA_HOME
 #export PATH=$PATH:$JAVA_HOME/bin
 
