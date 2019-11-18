52fc8114bb7f2a6a8b763bf7e78ab285d65b9b2b
wangzhenjun3 <wangzhenjun3@xdf.cn>
2019-11-18 16:19:38 +0800

    '提交代码'

--- src/main/java/study/springcloud/eureka/support/listener/EurekaListener.java
+++ src/main/java/study/springcloud/eureka/support/listener/EurekaListener.java
@@ -2,9 +2,7 @@ package study.springcloud.eureka.support.listener;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cloud.netflix.eureka.server.event.EurekaInstanceCanceledEvent;
-import org.springframework.cloud.netflix.eureka.server.event.EurekaInstanceRegisteredEvent;
-import org.springframework.cloud.netflix.eureka.server.event.EurekaInstanceRenewedEvent;
+import org.springframework.cloud.netflix.eureka.server.event.*;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
@@ -13,18 +11,50 @@ public class EurekaListener {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(EurekaListener.class);
 
+    /**
+     * 服务下线
+     */
     @EventListener
     public void listen(EurekaInstanceCanceledEvent event) {
+        event.getServerId();
+        event.getAppName();
         LOGGER.info("EurekaInstanceCanceledEvent {}", event.getAppName());
     }
 
+    /**
+     * 服务注册
+     */
     @EventListener
     public void listen(EurekaInstanceRegisteredEvent event) {
+        event.getInstanceInfo();
+        event.getLeaseDuration();
         LOGGER.info("EurekaInstanceRegisteredEvent {}", event.getInstanceInfo());
     }
 
+    /**
+     * 服务续约
+     */
     @EventListener
     public void listen(EurekaInstanceRenewedEvent event) {
+        event.getServerId();
+        event.getAppName();
+        event.getInstanceInfo();
         LOGGER.info("EurekaInstanceRegisteredEvent {}", event.getAppName());
+    }
+
+    /**
+     * 服务端启动事件
+     */
+    @EventListener
+    public void listen(EurekaRegistryAvailableEvent event) {
+        LOGGER.info("EurekaInstanceRegisteredEvent {}", event.getSource());
+    }
+
+    /**
+     * 服务端可用事件
+     */
+    @EventListener
+    public void listen(EurekaServerStartedEvent event) {
+        LOGGER.info("EurekaInstanceRegisteredEvent {}", event.getSource());
     }
 }
