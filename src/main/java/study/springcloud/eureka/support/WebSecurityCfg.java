7e9118e43ceca0b7acf7b1c4170b126517b134eb
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-02-27 17:02:05 +0800

    提交代码

--- src/main/java/study/springcloud/eureka/support/WebSecurityCfg.java
+++ src/main/java/study/springcloud/eureka/support/WebSecurityCfg.java
@@ -5,14 +5,15 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@EnableWebSecurity
 @Configuration
 public class WebSecurityCfg extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.httpBasic().disable();
-        super.configure(http);
+        http.httpBasic()
+                .disable();
+        http.csrf()
+                .disable();
+//        super.configure(http);
     }
 }
