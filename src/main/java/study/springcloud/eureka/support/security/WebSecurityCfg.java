78a8adeae5ddc590b5c5f003eb154018edd50512
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-10-23 09:53:06 +0800

    提交代码

--- src/main/java/study/springcloud/eureka/support/security/WebSecurityCfg.java
+++ src/main/java/study/springcloud/eureka/support/security/WebSecurityCfg.java
@@ -1,18 +1,18 @@
-package study.springcloud.eureka.support.security;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-//@Configuration
-public class WebSecurityCfg extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic()
-                .disable();
-        http.csrf()
-                .disable();
-//        super.configure(http);
-    }
-}
+//package study.springcloud.eureka.support.security;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+////@Configuration
+//public class WebSecurityCfg extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.httpBasic()
+//                .disable();
+//        http.csrf()
+//                .disable();
+////        super.configure(http);
+//    }
+//}
