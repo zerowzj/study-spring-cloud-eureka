696a5764f7d2e9c1cb50353e238677c1ccc2eda8
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-10-26 16:33:39 +0800

    提交代码

--- src/main/java/study/springcloud/eureka/support/SpringBootCfg.java
+++ src/main/java/study/springcloud/eureka/support/SpringBootCfg.java
@@ -3,7 +3,7 @@ package study.springcloud.eureka.support;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-//服务注册和发现
+//
 @EnableEurekaServer
 
 @SpringBootApplication(scanBasePackages = "study.springcloud.eureka")
